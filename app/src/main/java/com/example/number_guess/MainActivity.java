499228c9efcdd2097446eb061a7967d543ce76b6
Suchan Lee <suchanlee@Suchanui-Macmini.local>
2024-03-12 00:13:29 +0900

    add simple quiz

--- app/src/main/java/com/example/number_guess/MainActivity.java
+++ app/src/main/java/com/example/number_guess/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.number_guess;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +11,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
+    TextView tvLeft;
+    TextView tvRight;
+    TextView tvOperand;
+    Button btnCheck;
+    TextView tvAnswer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +30,49 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        tvLeft = findViewById(R.id.tv_left_number);
+        tvRight = findViewById(R.id.tv_right_number);
+        tvOperand = findViewById(R.id.tv_operand);
+
+        btnCheck = findViewById(R.id.button);
+        tvAnswer = findViewById(R.id.tv_right_answer);
+
+
+        btnCheck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String strAnswer = "";
+                int left = Integer.parseInt((String) tvLeft.getText());
+                int right = Integer.parseInt((String) tvRight.getText());
+                int nAnswer = 0;
+
+                if(tvOperand.getText().equals("+")){
+                    nAnswer = left + right;
+                    strAnswer = tvLeft.getText() + " + " + tvRight.getText() + " = " + nAnswer;
+                    tvAnswer.setText(strAnswer);
+                } else {
+                    nAnswer = left - right;
+                    strAnswer = tvLeft.getText() + " - " + tvRight.getText() + " = " + nAnswer;
+                    tvAnswer.setText(strAnswer);
+                }
+            }
+        });
+
+        setQuiz();
+    }
+
+    private void setQuiz() {
+        Random random = new Random();
+
+        String newNumber = "" + random.nextInt(999);
+        tvLeft.setText(newNumber);
+        newNumber = "" + random.nextInt(999);
+        tvRight.setText(newNumber);
+        int operand = random.nextInt(2);
+        switch(operand){
+            case 0 : tvOperand.setText("+"); break;
+            case 1 : tvOperand.setText("-"); break;
+        }
     }
 }
